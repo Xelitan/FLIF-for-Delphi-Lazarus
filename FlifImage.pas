02ee5c91ca501785ab626ba8edc1318b2b5aae30
Xelitan <140884394+Xelitan@users.noreply.github.com>
2025-02-08 22:08:14 +0100

    Add files via upload

--- FlifImage.pas
+++ FlifImage.pas
@@ -22,6 +22,7 @@ const LIBFLIF = 'libflif.dll';
 
 type
   PFLIF_DECODER = Pointer;
+  PFLIF_ENCODER = Pointer;
   PFLIF_IMAGE = Pointer;
 
   function flif_create_decoder: PFLIF_DECODER; cdecl; external LIBFLIF;
@@ -32,15 +33,26 @@ type
   function flif_image_get_width(image: PFLIF_IMAGE): UInt32; cdecl; external LIBFLIF;
   function flif_image_get_height(image: PFLIF_IMAGE): UInt32; cdecl; external LIBFLIF;
   procedure flif_image_read_row_RGBA8(image: PFLIF_IMAGE; row: UInt32; buffer: Pointer; buffer_size_bytes: Cardinal); cdecl; external LIBFLIF;
+  function flif_create_encoder: PFLIF_ENCODER; cdecl; external LIBFLIF;
+  procedure flif_destroy_encoder(encoder: PFLIF_ENCODER); cdecl; external LIBFLIF;
+  procedure flif_encoder_encode_file(encoder: PFLIF_ENCODER; const filename: PAnsiChar); cdecl; external LIBFLIF;
+  function flif_encoder_encode_memory(encoder: PFLIF_ENCODER; buffer: Pointer; out buffer_size_bytes: Cardinal): Integer; cdecl; external LIBFLIF;
+  procedure flif_encoder_add_image(encoder: PFLIF_ENCODER; image: PFLIF_IMAGE); cdecl; external LIBFLIF;
+  function flif_create_image(width, height: UInt32): PFLIF_IMAGE; cdecl; external LIBFLIF;
+  procedure flif_destroy_image(image: PFLIF_IMAGE); cdecl; external LIBFLIF;
+  procedure flif_image_write_row_RGBA8(image: PFLIF_IMAGE; row: UInt32; const buffer: Pointer; buffer_size_bytes: NativeUInt); cdecl; external LIBFLIF;
 
+  //set amount of quality loss, 0 for no loss, 100 for maximum loss, negative values indicate adaptive lossy (second image should be the saliency map)
+  procedure flif_encoder_set_lossy(encoder: PFLIF_ENCODER; loss: Integer); cdecl; external LIBFLIF;
 
   { TFlifImage }
 type
   TFlifImage = class(TGraphic)
   private
     FBmp: TBitmap;
+    FCompression: Integer;
     procedure DecodeFromStream(Str: TStream);
-    //procedure EncodeToStream(Str: TStream);
+    procedure EncodeToStream(Str: TStream);
   protected
     procedure Draw(ACanvas: TCanvas; const Rect: TRect); override;
   //    function GetEmpty: Boolean; virtual; abstract;
@@ -51,6 +63,8 @@ type
     procedure SetTransparent(Value: Boolean); override;
     procedure SetWidth(Value: Integer);override;
   public
+    procedure SetLossyCompression(Value: Cardinal);
+    procedure SetLosslessCompression;
     procedure Assign(Source: TPersistent); override;
     procedure LoadFromStream(Stream: TStream); override;
     procedure SaveToStream(Stream: TStream); override;
@@ -111,6 +125,54 @@ begin
   end;
 end;
 
+procedure TFlifImage.EncodeToStream(Str: TStream);
+var Encoder: PFLIF_ENCODER;
+    Image: PFLIF_IMAGE;
+    AWidth, AHeight: Integer;
+    Data: array of Byte;
+    x,y: Integer;
+    P: PByteArray;
+    OutData: array of Byte;
+    OutSize: Cardinal;
+begin
+  AWidth := FBmp.Width;
+  AHeight := FBmp.Height;
+
+  Encoder := flif_create_encoder;
+  if not Assigned(Encoder) then Exit;
+
+  try
+    Image := flif_create_image(AWidth, AHeight);
+    if not Assigned(Image) then Exit;
+
+    try
+      SetLength(Data, AWidth * 4);
+
+      for y:=0 to AHeight-1 do begin
+        P := Fbmp.ScanLine[y];
+
+        for x:=0 to AWidth-1 do begin
+          Data[4*x+2] := P[4*x  ]; //B
+          Data[4*x+1] := P[4*x+1]; //G
+          Data[4*x  ] := P[4*x+2]; //R
+          Data[4*x+3] := P[4*x+3]; //A
+        end;
+
+        flif_image_write_row_RGBA8(Image, y, @Data[0], AWidth * 4);
+      end;
+
+      flif_encoder_add_image(Encoder, Image);
+      flif_encoder_set_lossy(Encoder, FCompression);
+      flif_encoder_encode_memory(Encoder, @OutData, OutSize);
+      Str.Write(OutData[0], OutSize);
+    finally
+      flif_destroy_image(Image);
+    end;
+  finally
+    flif_destroy_encoder(Encoder);
+  end;
+end;
+
 procedure TFlifImage.Draw(ACanvas: TCanvas; const Rect: TRect);
 begin
   ACanvas.StretchDraw(Rect, FBmp);
@@ -146,6 +208,16 @@ begin
   FBmp.Width := Value;
 end;
 
+procedure TFlifImage.SetLossyCompression(Value: Cardinal);
+begin
+  FCompression := Value;
+end;
+
+procedure TFlifImage.SetLosslessCompression;
+begin
+  FCompression := 0;
+end;
+
 procedure TFlifImage.Assign(Source: TPersistent);
 var Src: TGraphic;
 begin
@@ -163,7 +235,7 @@ end;
 
 procedure TFlifImage.SaveToStream(Stream: TStream);
 begin
-  //
+  EncodeToStream(Stream);
 end;
 
 constructor TFlifImage.Create;
@@ -173,6 +245,7 @@ begin
   FBmp := TBitmap.Create;
   FBmp.PixelFormat := pf32bit;
   FBmp.SetSize(1,1);
+  FCompression := 0;
 end;
 
 destructor TFlifImage.Destroy;
